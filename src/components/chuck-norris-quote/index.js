6dc627ec1760a038d668f32ca01a52af66cb5ec7
Craig Pullar <craigpullar@Craigs-MacBook-Pro.local>
2018-12-29 10:40:54 -0500

    Use suspense and usefetch

--- src/components/chuck-norris-quote/index.js
+++ src/components/chuck-norris-quote/index.js
@@ -1,10 +1,16 @@
-import React from "react";
-import { useQuote } from "./hooks";
+import React, { Suspense } from "react";
+import useFetch from "fetch-suspense";
 
+const FetchingComponent = () => {
+  const quote = useFetch("https://api.chucknorris.io/jokes/random");
+  return <i>{quote.value}</i>;
+};
 const ChuckNorrisQuote = () => {
-  const { quote } = useQuote();
-
-  return <i>{quote}</i>;
+  return (
+    <Suspense fallback="Loading your chuck norris quote...">
+      <FetchingComponent />
+    </Suspense>
+  );
 };
 
 export default ChuckNorrisQuote;
