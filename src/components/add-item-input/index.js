eadb94ea46e48d3dd5e6dbe79f053ed017bed8cc
Craig Pullar <craigpullar@Craigs-MacBook-Pro.local>
2019-01-23 21:40:49 -0500

    remove add item title

--- src/components/add-item-input/index.js
+++ src/components/add-item-input/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import Input from "@material-ui/core/Input";
 import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 
 import { useInputValue } from "../../libs/hooks";
@@ -12,7 +11,6 @@ const AddItemInput = ({ addItem }) => {
 
   return (
     <Grid container style={{ padding: 40 }} direction="column" justify="center">
-      <Typography variant="h4">Add Item</Typography>
       <Grid container justify="center" style={{ padding: 20 }}>
         <Input type="text" value={value} onChange={onChange} />
         <Button
